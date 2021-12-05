ea6f3069fd4595c598cdae94e6a0e4a4f0df0485
Joachim <joachim.robert@protonmail.com>
2021-12-05 13:40:18 +0100

    Update packed assets

---
+++ static/gen/packed.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",function(){var scrollEl=document.querySelector("th.cell_past ~ th:not(.cell_past)");if(!scrollEl){return;}
+var scrollElWrapper=scrollEl.closest(".table-container");scrollElWrapper.scroll({left:scrollEl.offsetLeft-scrollElWrapper.offsetWidth/2,behavior:'smooth'});});document.addEventListener("DOMContentLoaded",function(){var locationInput=document.getElementById("location-input");var locationsList=document.getElementById("locations-list");locationInput.onfocus=function(e){e.target.value="";}
+locationInput.onchange=function(){document.getElementById("location-form").submit();locationInput.disabled=true;}
+locationInput.oninput=function(e){if(!window.fetch)return;window.fetch("https://geo.api.gouv.fr/communes?nom="+e.target.value+"&boost=population&limit=5").then(function(r){return r.json()}).then(function(data){if(!data.length)return
+locationsList.innerHTML=data.map(l=>{return"<option value='"+l.nom+"'></option>";})});}});
